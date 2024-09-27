dae4971644b8eff6945a1b424d78a2a3d9d31222
Jonathan Kammerland <jkammerland@gmail.com>
2024-09-27 22:33:28 +0200

    Add string_view deserial without data_in_out<char>

--- test/src/test_zero_copy_byte_types.cpp
+++ test/src/test_zero_copy_byte_types.cpp
@@ -1,5 +1,6 @@
 #include "test.h"
 #include "gtest/gtest.h"
+#include <sys/types.h>
 
 namespace test_zero_copy_byte_types {
 
@@ -88,30 +89,62 @@ TEST(test_zero_copy_byte_types, plain_string_view) {
   auto [dataIn, in] = zpp::bits::data_in();
 
   // Data to transfer
-  std::string_view s{"This is a string view"};
+  std::string_view s1{"This is a string view"};
 
   // Encode data
-  auto result = out(s);
-  EXPECT_TRUE(!failure(result));
+  auto result = out(s1);
+  EXPECT_FALSE(failure(result));
 
   // Emulate data transfer
   dataIn = data;
 
-  // Decode data (I would like to use string_view as type here, not
-  // std::span<const std::byte>)
+  // Decode data
   std::string_view s2;
   result = in(s2);
   EXPECT_TRUE(!failure(result));
 
   // Check data
-  EXPECT_TRUE(s == s2);
+  EXPECT_EQ(s1, s2);
 
   // Check data location is different (it was truly transferred)
-  EXPECT_TRUE(s.data() != s2.data());
+  EXPECT_NE(s1.data(), s2.data());
 
+  // Check data location is within the "in" buffer
   EXPECT_TRUE(s2.data() >= reinterpret_cast<const char *>(dataIn.data()));
   EXPECT_TRUE(s2.data() <
               reinterpret_cast<const char *>(dataIn.data() + dataIn.size()));
+}
+
+TEST(test_zero_copy_byte_types, combination_views) {
+  auto [data, out] = zpp::bits::data_out();
+  auto [dataIn, in] = zpp::bits::data_in();
+
+  struct data_view {
+    std::string_view s;
+    std::span<const std::byte> v;
+  };
+
+  // Data to transfer
+  std::string s1{"hello"};
+  std::vector<std::byte> v1 = {std::byte{'w'}, std::byte{'o'}, std::byte{'r'},
+                               std::byte{'l'}, std::byte{'d'}};
+  data_view d1{s1, v1};
+
+  // Encode data
+  auto result = out(d1);
+
+  // Emulate data transfer
+  dataIn = data;
+
+  // Decode data
+  data_view d2;
+  result = in(d2);
+
+  // Check data
+  EXPECT_EQ(d1.s, d2.s);
+  EXPECT_NE(d1.v.data(), d2.v.data());
+  EXPECT_EQ(d1.v.size(), d2.v.size());
+  EXPECT_EQ(std::memcmp(d1.v.data(), d2.v.data(), d1.v.size()), 0);
 }
 
 TEST(test_zero_copy_byte_types, unsized_string_view) {
